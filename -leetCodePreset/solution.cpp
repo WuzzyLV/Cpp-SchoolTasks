8b4654809aca105712fd30a8abaae44ce6804e99
WuzzyLV <54778608+WuzzyLV@users.noreply.github.com>
2023-02-09 09:06:34 +0000

    adding letcode preset

---
+++ -leetCodePreset/solution.cpp
@@ -0,0 +1,42 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    int maximumWealth(vector<vector<int>>& accounts) {
+
+    }
+};
+
+//////////// manual testing //////////
+
+int print2DVec(vector<vector<int>> vec){
+    for(vector<int> i : vec){
+        for(int val : i){
+            cout<< val << " ";
+        }
+        cout <<endl;
+    }
+    cout <<endl;
+}
+int printVec(vector<int> vec){
+    for(int i : vec){
+        cout<< i << " ";
+    }
+    cout <<endl;
+}
+
+int main(int argc, char const *argv[]){
+    Solution sol;
+    /// ///// //// ////
+
+
+    cout <<sol.xxxx() << endl;
+    
+
+    return 0;
+}
+
+/////////////////////////////////////
